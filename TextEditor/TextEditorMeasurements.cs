f902672f04a2760f55ed5355fd2e3d2909ae6459
Luthetus <luthetus@gmail.com>
2026-01-12 12:58:44 -0500

    More measurements

--- TextEditor/TextEditorMeasurements.cs
+++ TextEditor/TextEditorMeasurements.cs
@@ -1,7 +1,16 @@
 namespace TextEditor;
 
+/// <summary>All measurements are in pixels unless otherwise stated</summary>
 public struct TextEditorMeasurements
 {
     public double CharacterWidth { get; set; }
     public int LineHeight { get; set; }
+    public int EditorWidth { get; set; }
+    public int EditorHeight { get; set; }
+    public int EditorLeft { get; set; }
+    public int EditorTop { get; set; }
+    /// <summary>"literal" as opposed to the "scrollHeight", this is the amount of width the y-axis scrollbar measures</summary>
+    public double ScrollbarLiteralWidth { get; set; }
+    /// <summary>"literal" as opposed to the "scrollWidth", this is the amount of height the x-axis scrollbar measures</summary>
+	public double ScrollbarLiteralHeight { get; set; }
 }
--- TextEditor/wwwroot/textEditor.js
+++ TextEditor/wwwroot/textEditor.js
@@ -9,29 +9,47 @@ window.ideTextEditor = {
         }
     },
     getTextEditorMeasurements: function () {
-        let textEditorElement = document.getElementById("te_component-id");
+		let textEditorElement = document.getElementById("te_component-id");
 
         if (!textEditorElement) {
             return {
                 CharacterWidth: 5,
-                LineHeight: 5
+                LineHeight: 5,
+                EditorWidth: 0,
+                EditorHeight: 0,
+				BoundingClientRectLeft: 0,
+				BoundingClientRectTop: 0,
             }
         }
         
-        let child = document.createElement("div");
-        textEditorElement.appendChild(child);
+        let boundingClientRect = textEditorElement.getBoundingClientRect();
         
+        let measureTextElement = document.createElement("div");
+        textEditorElement.appendChild(measureTextElement);
         let elevenTimesAlphabetAndDigits = "abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789";
-        child.innerText = elevenTimesAlphabetAndDigits;
+        measureTextElement.innerText = elevenTimesAlphabetAndDigits;
+        let characterWidth = measureTextElement.offsetWidth / elevenTimesAlphabetAndDigits.length;
+        let lineHeight = measureTextElement.offsetHeight;
+        textEditorElement.removeChild(measureTextElement);
         
-        let characterWidth = child.offsetWidth / elevenTimesAlphabetAndDigits.length;
-        let lineHeight = child.offsetHeight;
-        
-        textEditorElement.removeChild(child);
+        // "literal" as opposed to the "scrollHeight", this is the amount of width the y-axis scrollbar measures
+        // "literal" as opposed to the "scrollWidth", this is the amount of height the x-axis scrollbar measures
+        let measureScrollbarLiteralElement = document.createElement("div");
+        textEditorElement.appendChild(measureScrollbarLiteralElement);
+        measureScrollbarLiteralElement.setAttribute('style', "width:0; height:0; overflow:scroll;");
+        let scrollbarLiteralWidth = Math.ceil(elementReference.offsetWidth);
+        let scrollbarLiteralHeight = Math.ceil(elementReference.offsetHeight),
+        textEditorElement.removeChild(measureScrollbarLiteralElement);
 
         return {
             CharacterWidth: characterWidth,
-            LineHeight: lineHeight
+            LineHeight: lineHeight,
+            EditorWidth: Math.ceil(textEditorElement.offsetWidth),
+            EditorHeight: Math.ceil(textEditorElement.offsetHeight),
+            EditorLeft: boundingClientRect.left,
+			EditorTop: boundingClientRect.top,
+			ScrollbarLiteralWidth: scrollbarLiteralWidth,
+			ScrollbarLiteralHeight: scrollbarLiteralHeight,
         }
     },
 }
