0eae83519e96857dcb09ef7ea17c58cf10cd0ea7
Luthetus <luthetus@gmail.com>
2026-01-15 14:11:12 -0500

    Update TextEditorComponent.razor.cs

--- TextEditor/TextEditorComponent.razor.cs
+++ TextEditor/TextEditorComponent.razor.cs
@@ -326,10 +326,12 @@ public sealed partial class TextEditorComponent : ComponentBase, IDisposable
             if (positionIndex > Model.SelectionAnchor && !anchorIsLessThanEnd ||
                 positionIndex < Model.SelectionAnchor && anchorIsLessThanEnd)
             {
-                (Model.SelectionAnchor, Model.SelectionEnd) = (Model.SelectionEnd, Model.SelectionAnchor);
-                Model.PositionIndex = Model.SelectionEnd;
-                (Model.LineIndex, Model.ColumnIndex) = Model.GetLineColumnIndices(Model.PositionIndex);
+                Model.SelectionAnchor = Model.SelectionEnd;
             }
+
+            Model.SelectionEnd = positionIndex;
+            Model.PositionIndex = positionIndex;
+            (Model.LineIndex, Model.ColumnIndex) = (lineIndex, columnIndex);
 
             StateHasChanged();
         }
